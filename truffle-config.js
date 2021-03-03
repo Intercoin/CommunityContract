19761e91c7f34eeb6c3976b1b1b358430e69456d
Artem Subbotin <subbotin.artem@gmail.com>
2021-03-03 09:44:38 +0200

    restored package.json

--- truffle-config.js
+++ truffle-config.js
@@ -52,7 +52,7 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
-     gas: 8000000
+     gas: 9000000
     },
     // Another network with more advanced options...
     // advanced: {
@@ -91,13 +91,13 @@ module.exports = {
     solc: {
       version: "0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
       //  evmVersion: "byzantium"
-      // }
+      }
     },
   },
 };
