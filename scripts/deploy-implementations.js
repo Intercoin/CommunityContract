27c5720bdc9da4a1271547fb42e0615b1b26df6f
Artem Subbotin <subbotin.artem@gmail.com>
2022-07-29 14:41:19 +0300

    imporved deploy scripts

--- scripts/deploy-implementations.js
+++ scripts/deploy-implementations.js
@@ -1,7 +1,50 @@
-async function main() {
+const fs = require('fs');
+//const HDWalletProvider = require('truffle-hdwallet-provider');
 
-	//const [deployer] = await ethers.getSigners();
-	const [,deployer] = await ethers.getSigners();
+function get_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./scripts/arguments.json', (err, data) => {
+            if (err) {
+				
+                if (err.code == 'ENOENT' && err.syscall == 'open' && err.errno == -4058) {
+                    fs.writeFile('./scripts/arguments.json', "", (err2) => {
+                        if (err2) throw err2;
+                        resolve();
+                    });
+                    data = ""
+                } else {
+                    throw err;
+                }
+            }
+    
+            resolve(data);
+        });
+    });
+}
+
+function write_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.writeFile('./scripts/arguments.json', _message, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+            resolve();
+        });
+    });
+}
+
+async function main() {
+	var data = await get_data();
+
+    var data_object_root = JSON.parse(data);
+	var data_object = {};
+	if (typeof data_object_root[hre.network.name] === 'undefined') {
+        data_object.time_created = Date.now()
+    } else {
+        data_object = data_object_root[hre.network.name];
+    }
+	//----------------
+
+	const [deployer] = await ethers.getSigners();
 	
 	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 	console.log(
@@ -9,10 +52,10 @@ async function main() {
 		deployer.address
 	);
 
-	var options = {
-		//gasPrice: ethers.utils.parseUnits('50', 'gwei'), 
-		gasLimit: 10e6
-	};
+	// var options = {
+	// 	//gasPrice: ethers.utils.parseUnits('50', 'gwei'), 
+	// 	gasLimit: 10e6
+	// };
 
 	console.log("Account balance:", (await deployer.getBalance()).toString());
 
@@ -28,6 +71,20 @@ async function main() {
 	console.log("  Community deployed at:       ", implementationCommunity.address);
 	console.log("  CommunityState deployed at:  ", implementationCommunityState.address);
 	console.log("  CommunityView deployed at:   ", implementationCommunityView.address);
+
+	data_object.implementationCommunity 	= implementationCommunity.address;
+	data_object.implementationCommunityState= implementationCommunityState.address;
+	data_object.implementationCommunityView	= implementationCommunityView.address;
+
+
+	//---
+	const ts_updated = Date.now();
+    data_object.time_updated = ts_updated;
+    data_object_root[`${hre.network.name}`] = data_object;
+    data_object_root.time_updated = ts_updated;
+    let data_to_write = JSON.stringify(data_object_root, null, 2);
+	console.log(data_to_write);
+    await write_data(data_to_write);
 }
 
 main()
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,48 @@
-async function main() {
+const fs = require('fs');
+//const HDWalletProvider = require('truffle-hdwallet-provider');
 
-	//const [deployer] = await ethers.getSigners();
-	const [,deployer] = await ethers.getSigners();
+function get_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./scripts/arguments.json', (err, data) => {
+            if (err) {
+                if (err.code == 'ENOENT' && err.syscall == 'open' && err.errno == -4058) {
+					let obj = {};
+					data = JSON.stringify(obj, null, "");
+                    fs.writeFile('./scripts/arguments.json', data, (err) => {
+                        if (err) throw err;
+                        resolve(data);
+                    });
+                } else {
+                    throw err;
+                }
+            } else {
+            	resolve(data);
+			}
+        });
+    });
+}
+
+async function main() {
+	var data = await get_data();
+    var data_object_root = JSON.parse(data);
+	if (typeof data_object_root[hre.network.name] === 'undefined') {
+		throw("Arguments file: missed data");
+    } else if (typeof data_object_root[hre.network.name] === 'undefined') {
+		throw("Arguments file: missed network data");
+    }
+	data_object = data_object_root[hre.network.name];
+	if (
+		typeof data_object.implementationCommunity === 'undefined' ||
+		typeof data_object.implementationCommunityState === 'undefined' ||
+		typeof data_object.implementationCommunityView === 'undefined' ||
+		!data_object.implementationCommunity ||
+		!data_object.implementationCommunityState ||
+		!data_object.implementationCommunityView
+	) {
+		throw("Arguments file: wrong addresses");
+	}
+
+	const [deployer] = await ethers.getSigners();
 	
 	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 	console.log(
@@ -13,11 +54,13 @@ async function main() {
 		//gasPrice: ethers.utils.parseUnits('50', 'gwei'), 
 		gasLimit: 5e6
 	};
-
+	let _params = [
+		data_object.implementationCommunity,
+		data_object.implementationCommunityState,
+		data_object.implementationCommunityView
+	]
 	let params = [
-		"0x29F393A1663e23b159bEd0dc1eE27B7Ded8D2400",
-  		"0x1eCE0E8B5cD7eDEeD8DA1fC24Cd082A6Be8F2A77",
-  		"0xEb5959A1D8E7e02aadF60B1a758B5607e4939C93",
+		..._params,
 		options
 	]
 
@@ -28,6 +71,7 @@ async function main() {
 	this.factory = await CommunityF.connect(deployer).deploy(...params);
 
 	console.log("Factory deployed at:", this.factory.address);
+	console.log("with params:", [..._params]);
 }
 
 main()
