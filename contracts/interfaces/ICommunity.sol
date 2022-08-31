b2b6b42bdf4b6e81758facd101160b5070a40a1a
Gregory Magarshak <greg@qbix.com>
2022-08-31 18:22:06 -0400

    Update ICommunity.sol
    Refactor for estimating lines of code

--- contracts/interfaces/ICommunity.sol
+++ contracts/interfaces/ICommunity.sol
@@ -1,15 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
-
 interface ICommunity {
-    
-    function initialize(
-        address implState,
-        address implView,
-        address hook, 
-        address costManager, 
-        string memory name, 
-        string memory symbol
-    ) external;
-    
-}
+    function initialize(address implState, address implView, address hook, address costManager, string memory name, string memory symbol) external; }
