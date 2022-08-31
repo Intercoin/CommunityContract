9cabb26db7bcfd797afb12e3be335110dbcc67c7
Gregory Magarshak <greg@qbix.com>
2022-08-31 18:22:44 -0400

    Update ICommunityHook.sol
    Refactor for estimating lines of code

--- contracts/interfaces/ICommunityHook.sol
+++ contracts/interfaces/ICommunityHook.sol
@@ -1,13 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-
-/**
-* @title interface represents hook contract that can be called every time when role created/granted/revoked
-*/
+import "@openzeppelin/contracts/utils/introspection/IERC165.
 interface ICommunityHook  is IERC165 {
     function roleGranted(bytes32 role, uint8 roleIndex, address account) external;
     function roleRevoked(bytes32 role, uint8 roleIndex, address account) external;
-    function roleCreated(bytes32 role, uint8 roleIndex) external;
-
-}
+    function roleCreated(bytes32 role, uint8 roleIndex) external; }
