bb7b306b55abb10a8b14c25e14262f8ef46e25c9
Artem Subbotin <subbotin.artem@gmail.com>
2022-09-07 19:02:25 +0300

    was updated interface for linking with ControlContract

--- contracts/interfaces/ICommunity.sol
+++ contracts/interfaces/ICommunity.sol
@@ -12,4 +12,7 @@ interface ICommunity {
         string memory symbol
     ) external;
     
+    function addressesCount(uint8 roleIndex) external view returns(uint256);
+    function getRoles(address member)external view returns(uint8[] memory);
+    function getAddresses(uint8 rolesIndex) external view returns(address[] memory);
 }
